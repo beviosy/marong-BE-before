d09e2768dc0d92fb6dca1be53dc2d4f1c7ad92dd
beviosy <cjstk0021@gmail.com>
2025-04-30 01:18:32 +0900

    Add Spring Boot project structure

---
+++ src/main/java/com/ktb/marong/MarongApplication.java
@@ -0,0 +1,13 @@
+package com.ktb.marong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ktb/marong/MarongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb.marong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
